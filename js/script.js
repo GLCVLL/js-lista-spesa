15ffcb3d4ac7b92ccb1936e67fdee28294515ab0
GLCVLL <g.ioele96@outlook.it>
2023-06-05 17:29:43 +0200

    added list from js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 console.log('JS OK');
+
+// RECUPERO ELEMENTO DAL DOM
+
+const listField = document.getElementById("shopping-list");
+console.log(listField);
+
+// CREO LISTA SPESA
+
+const shoppingList = ["mele", "banane", "pane", "latte", "uova"];
+
+// CREO WHILE LOOP
+
+let list = '';
+
+let i = 0;
+
+while (i < shoppingList.length) {
+
+  list += `<li>${shoppingList[i]}</li>`;
+
+  i++;
+}
+
+// STAMPO IN PAGINA
+
+listField.innerHTML = list;
